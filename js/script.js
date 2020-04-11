068537f743c7f32b5415545f8e6a5e6234c2ed55
Anastasya <AnastasyaShapovalova>
2020-04-11 16:56:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let mySlider = document.querySelector(".custom_slider");
+mySlider.customSlider({
+    maxSlides: 1,
+    slideWidth: 300,
+    slideHeight: 200,
+    loop: false,
+    navs: true,
+    autoplay: true,
+    timeout: 400,
+});
